99a1bb6f349c77f8df6f164f3134f510e6ea794e
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2024-09-21 21:51:00 +0800

    log var in version endpoint

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './common/decorators';
+import { EnvironmentVariableUtil } from './common/utils/environment-variable.util';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly envVarUtil: EnvironmentVariableUtil,
+  ) {}
 
   @Get('/')
   @Public()
@@ -15,6 +19,10 @@ export class AppController {
   @Get('/version')
   @Public()
   async checkVersion(): Promise<{ version: string }> {
+    /**@todo remove*/
+    Logger.log({
+      envVar: this.envVarUtil.getVariables(),
+    });
     return await this.appService.getVersion();
   }
 }
