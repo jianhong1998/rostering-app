444f6fdcfe56b7bac0a86ef7ca220a8a7eacc6ab
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2024-09-21 21:49:20 +0800

    public version endpoint

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/decorators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/')
+  @Public()
   async getHello(): Promise<void> {
     await this.appService.healthCheck();
   }
 
   @Get('/version')
+  @Public()
   async checkVersion(): Promise<{ version: string }> {
     return await this.appService.getVersion();
   }
